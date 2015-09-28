6b54dde2fa00d3a0e0b35e71eeb462243ae6e98e
mckshreder <garrettmckenzie@ymail.com>
2015-09-27 21:09:37 -0700

    first commit

---
+++ app/controllers/main_views_controller.rb
@@ -0,0 +1,29 @@
+class MainViewsController < ApplicationController
+  
+  def blog_single
+  end
+
+  def blog
+  end
+
+  def cart
+  end
+
+  def checkout
+  end
+
+  def contact_us
+  end
+
+  def index
+  end
+
+  def login
+  end
+
+  def product_details
+  end
+
+  def shop
+  end
+end
---
+++ app/helpers/main_views_helper.rb
@@ -0,0 +1,2 @@
+module MainViewsHelper
+end
